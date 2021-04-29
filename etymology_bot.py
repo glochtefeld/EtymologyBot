55aab7c96acf7d19c51773e492104bc6d444d514
Gavin <gavin.loch@gmail.com>
2021-04-29 17:31:48 -0500

    added support for custom greetings

--- etymology_bot.py
+++ etymology_bot.py
@@ -4,12 +4,19 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 from dotenv import dotenv_values
+from random import choice
 
 config = dotenv_values(".env")
 
 logging.basicConfig(level=logging.INFO)
 
 bot = commands.Bot(command_prefix='!@')
+
+def get_random_greeting():
+    with open(config["GREETINGS_PATH"],'r') as infile:
+        greetings = infile.readlines()
+        return choice(greetings)
+
 @bot.event
 async def on_ready():
     logging.info('Logged in as {0.user}'.format(bot))
@@ -17,14 +24,17 @@ async def on_ready():
 @bot.command()
 async def etym(ctx, word: str):
     url = "http://www.etymonline.com/word/" + word
+    logging.info(f"Looking up {word}")
     page = requests.get(url)
     if page.status_code != 200:
+        logging.error(f"Unknown word: {word}")
         return
     soup = BeautifulSoup(page.content, 'html.parser')
     paragraphs = soup.find_all('p')
     
     i = 1
     msg = "\u200B"
+    msg += get_random_greeting()
     definition = paragraphs[0]
     msg += definition.get_text()
     while True:
