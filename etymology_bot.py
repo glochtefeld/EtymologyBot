65ef98d442861a1e9f52803faf6c5625d55e0be9
Gavin Lochtefeld <gavin.loch@gmail.com>
2021-05-28 16:06:32 -0500

    added command to specify bot channel

--- etymology_bot.py
+++ etymology_bot.py
@@ -2,12 +2,13 @@ import discord
 from discord.ext import commands
 import logging
 import requests
+import re
 from bs4 import BeautifulSoup
 from dotenv import dotenv_values
 from random import choice
 
 config = dotenv_values(".env")
-
+G_CHANNEL = ""
 logging.basicConfig(level=logging.INFO)
 
 bot = commands.Bot(command_prefix='!@')
@@ -44,6 +45,27 @@ async def etym(ctx, word: str):
         i += 2
     msg += f"\n<{url}>"
 
-    await ctx.send(msg)
+    if G_CHANNEL == "":
+        await ctx.send(msg)
+    else:
+        await G_CHANNEL.send(msg)
+
+@bot.command(name='etym_bot_channel')
+async def set_channel(ctx, msg_body: str):
+    global G_CHANNEL
+    logging.info(f'Message body: {msg_body}')
+    desired_channel = re.search("<#\d+>",msg_body)
+    if desired_channel:
+        # slicing prevents characters like <,#,> from being parsed
+        channel = int(desired_channel.group()[2:-1])
+        print(f"CHANNEL: {channel}")
+        G_CHANNEL = bot.get_channel(channel)
+        print(type(G_CHANNEL))
+        await G_CHANNEL.send("All messages from this bot will arrive here now.")
+    else:
+        await ctx.reply("Please specify a channel to send messages to.")
+
+
+
 
 bot.run(config["DISCORD_TOKEN"])
